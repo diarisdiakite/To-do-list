684f983053f348f3659d096a0444dfec8c4f9663
diarisdiakite <diarisdiakite@gmail.com>
2023-05-17 20:54:34 +0200

    Fix Eslint issue

--- src/modules/deleteAllCompleted.js
+++ src/modules/deleteAllCompleted.js
@@ -2,8 +2,8 @@ import { todaysList, tasks } from './taskList.js';
 
 const deleteCompletedTasks = () => {
   const incompleteTasks = tasks.filter((task) => !task.completed);
-  tasks.length = 0; 
-  tasks.push(...incompleteTasks); 
+  tasks.length = 0;
+  tasks.push(...incompleteTasks);
   todaysList.saveTasksToLocalStorage();
 };
 
