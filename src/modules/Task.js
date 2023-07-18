14f8351d3fee108a86784393cccee4f44dceb861
diarisdiakite <diarisdiakite@gmail.com>
2023-07-18 02:47:50 +0200

    Update Task.js class

---
+++ src/modules/Task.js
@@ -0,0 +1,7 @@
+export default class Task {
+    constructor(description, completed = false, id) {
+      this.description = description;
+      this.completed = completed;
+      this.id = id;
+    }
+  }
--- src/modules/task.js
+++
@@ -1,7 +0,0 @@
-export default class Task {
-  constructor(description, completed = false, id) {
-    this.description = description;
-    this.completed = completed;
-    this.id = id;
-  }
-}
