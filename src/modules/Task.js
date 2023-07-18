0e6d2921f40f86692b6edd175e4600c21fcab5f0
diarisdiakite <diarisdiakite@gmail.com>
2023-07-18 02:51:01 +0200

    Fix Eslint

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,7 +1,7 @@
 export default class Task {
-    constructor(description, completed = false, id) {
-      this.description = description;
-      this.completed = completed;
-      this.id = id;
-    }
+  constructor(description, completed = false, id) {
+    this.description = description;
+    this.completed = completed;
+    this.id = id;
   }
+}
